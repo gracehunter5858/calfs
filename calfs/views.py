407f2606e1768374acb9391a0a03f3e3e1263c9e
Grace Hunter <gracehunter@Graces-MacBook.local>
2018-10-13 23:02:53 -0700

    contact page updates

--- calfs/views.py
+++ calfs/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 def home(request):
     return render(request, 'home.html', {})
@@ -13,4 +16,15 @@ def gallery(request):
     return render(request, 'gallery.html', {})
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            sender_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            send_mail(subject, message, 'ucbfigureskating@gmail.com', ['ucbfigureskating@gmail.com'])
+            return HttpResponse('Thanks for contacting us!')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
